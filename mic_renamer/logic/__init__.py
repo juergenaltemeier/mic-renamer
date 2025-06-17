9e58af97a833c1ae136ac146d94c63ea50478610
Jürgen Altemeier <48452891+juergenaltemeier@users.noreply.github.com>
2025-06-17 18:09:34 +0200

    feat: add undo rename functionality

--- mic_renamer/logic/__init__.py
+++ mic_renamer/logic/__init__.py
@@ -3,5 +3,6 @@
 from .renamer import Renamer
 from .image_compressor import ImageCompressor
 from .heic_converter import convert_heic
+from .undo_manager import UndoManager
 
-__all__ = ["Renamer", "ImageCompressor", "convert_heic"]
+__all__ = ["Renamer", "ImageCompressor", "convert_heic", "UndoManager"]
